3275acaa5b8e65c98eaabd5b4145932a5d2ff7a7
Erik Fernandes <oerickfernandes@gmail.com>
2023-07-21 17:32:05 -0300

    Primeiro teste de comunicaçao entre o banco de dados, .NET e a View

---
+++ SistemaMatricula.mvc/SistemaMatricula.mvc/Controllers/MatriculasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaMatricula.mvc.Database;
+using SistemaMatricula.mvc.Models;
+
+namespace SistemaMatricula.mvc.Controllers
+{
+    public class MatriculasController : Controller
+    {
+        private readonly MatriculaContext _db;
+
+        public MatriculasController(MatriculaContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //Salvando toda a tabela do banco de dados em uma Enumerable e passando esse para a view
+            IEnumerable<Aluno> aluno = _db.Matriculas;
+            return View(aluno);
+        }
+    }
+}
