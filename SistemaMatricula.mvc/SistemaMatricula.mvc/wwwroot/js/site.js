5da6afa0c273a93c7e91c9cf2aacc530374265b1
Erik Fernandes <oerickfernandes@gmail.com>
2023-07-23 19:59:30 -0300

    Configurando os filtros via DataTables Jquery

--- SistemaMatricula.mvc/SistemaMatricula.mvc/wwwroot/js/site.js
+++ SistemaMatricula.mvc/SistemaMatricula.mvc/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+//Jquery fazendo referencia ao nosso documento, quando estiver pronto, irá executar o codigo abaixo.
+$(document).ready(function () {
+    $('#matriculas').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ de _END_ de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "Processing...",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Ultimo",
+                "next": "Proximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
+    });
+});
